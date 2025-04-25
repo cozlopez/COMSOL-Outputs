9d93505fb28d0494cdff1ffc2050f5eb526a8e29
cozlopez <c.orcelopez@student.tudelft.nl>
2025-04-25 13:59:44 +0200

    added files

---
+++ SoundLevelmain.py
@@ -0,0 +1,8 @@
+from src.SPL_directivity_arc import SPL_directivity_input_data
+
+
+
+data = SPL_directivity_input_data("Untitled2.txt",8500, sweep = True) # No sweep
+
+for i in range(5):
+    print("test")
